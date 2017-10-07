4cd412db5249a83d207863a9d89cad1b493ea212
Chadtech <chadstearns1@gmail.com>
2017-10-07 21:02:35 +0200

    Added menu buttons and tooltips

--- source/Toolbar.elm
+++ source/Toolbar.elm
@@ -1,32 +1,61 @@
 module Toolbar exposing (view)
 
 import Html exposing (Html, a, div, text)
-import Html.Attributes exposing (class, classList)
+import Html.Attributes exposing (attribute, class, classList, title)
 import Html.Events exposing (onClick)
 import Tool exposing (Tool(..))
-import Types exposing (Model, Msg(..))
-import Util exposing ((:=))
+import Types exposing (Command(..), Model, Msg(..))
+import Util exposing ((&), (:=))
 
 
 view : Model -> Html Msg
 view model =
     div
         [ class "vertical-tool-bar" ]
-        (List.map (buttonView model.tool) Tool.all)
+        (children model.tool)
 
 
 
 -- COMPONENTS --
 
 
-buttonView : Tool -> Tool -> Html Msg
-buttonView currentTool thisButtonsTool =
+children : Tool -> List (Html Msg)
+children currentTool =
+    [ List.map (toolButton currentTool) Tool.all
+    , List.map menuButton menus
+    ]
+        |> List.concat
+
+
+menus : List ( String, String, Command )
+menus =
+    [ ( "\xEA19", "text", InitText )
+    , ( "\xEA0F", "replace color", InitReplaceColor )
+    , ( "\xEA0E", "invert colors", InvertColors )
+    ]
+
+
+menuButton : ( String, String, Command ) -> Html Msg
+menuButton ( icon, name, command ) =
+    a
+        [ class "tool-button"
+        , onClick (Command command)
+        , attribute "data-toggle" "tooltip"
+        , title name
+        ]
+        [ text icon ]
+
+
+toolButton : Tool -> Tool -> Html Msg
+toolButton currentTool thisButtonsTool =
     a
         [ classList
             [ "tool-button" := True
             , "selected"
                 := isCurrentTool currentTool thisButtonsTool
             ]
+        , attribute "data-toggle" "tooltip"
+        , title (Tool.name thisButtonsTool)
         , onClick (SetTool thisButtonsTool)
         ]
         [ text (Tool.icon thisButtonsTool) ]
