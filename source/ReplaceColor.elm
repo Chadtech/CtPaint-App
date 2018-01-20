59fc4ca694308b1802bae92bacf5405ee5aa5c69
Chadtech <chadstearns1@gmail.com>
2018-01-20 01:28:05 +0100

    two sections in replace color menu now with hex fields

--- source/ReplaceColor.elm
+++ source/ReplaceColor.elm
@@ -3,6 +3,7 @@ module ReplaceColor exposing (..)
 import Chadtech.Colors
     exposing
         ( backgroundx2
+        , ignorable0
         , ignorable1
         , ignorable3
         )
@@ -10,10 +11,11 @@ import Color exposing (Color)
 import Css exposing (..)
 import Css.Elements
 import Css.Namespace exposing (namespace)
-import Html exposing (Html, a, div, p, text)
+import Html exposing (Html, a, div, input, p, text)
+import Html.Attributes exposing (spellcheck, value)
 import Html.CssHelpers
 import Html.Custom exposing (indent)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 import Reply exposing (Reply(NoReply, Replace))
 import Tuple.Infix exposing ((&))
 import Util
@@ -22,11 +24,17 @@ import Util
 type Msg
     = ReplaceButtonClicked
     | SquareClicked Color.Color
+    | UpdateReplacementHexField String
+    | UpdateTargetHexField String
 
 
 type alias Model =
     { target : Color.Color
     , replacement : Color.Color
+    , targetHexField : String
+    , replacementHexField : String
+    , targetPaletteIndex : Maybe Int
+    , replacementPaletteIndex : Maybe Int
     , palette : List Color.Color
     }
 
@@ -45,23 +53,33 @@ update msg model =
             { model | replacement = color }
                 & NoReply
 
+        UpdateReplacementHexField str ->
+            model & NoReply
+
+        UpdateTargetHexField str ->
+            model & NoReply
+
 
 
 -- STYLES --
 
 
 type Class
-    = ColorRow
+    = Row
     | Palette
     | Square
     | ColorView
     | ReplaceButton
+    | Section
+    | With
+    | HexField
+    | Selected
 
 
 css : Stylesheet
 css =
-    [ Css.class ColorRow
-        [ height (px 20)
+    [ Css.class Row
+        [ height (px 24)
         , marginBottom (px 4)
         , children
             [ Css.Elements.p
@@ -71,10 +89,15 @@ css =
                 ]
             ]
         ]
+    , Css.class Section
+        [ display inlineBlock
+        , withClass With
+            [ marginLeft (px 8) ]
+        ]
     , (Css.class ColorView << List.append indent)
         [ display inlineBlock
-        , width (px 86)
-        , height (px 16)
+        , width (px 102)
+        , height (px 20)
         ]
     , (Css.class Palette << List.append indent)
         [ width (px 176)
@@ -90,9 +113,22 @@ css =
         , height (px 20)
         , width (px 20)
         , float left
+        , withClass Selected
+            [ borderLeft3 (px 2) solid ignorable0
+            , borderTop3 (px 2) solid ignorable0
+            , borderRight3 (px 2) solid ignorable0
+            , borderBottom3 (px 2) solid ignorable0
+            , height (px 18)
+            , width (px 18)
+            ]
         ]
     , Css.class ReplaceButton
         [ marginTop (px 8) ]
+    , Css.class HexField
+        [ width (px 70)
+        , marginLeft (px 4)
+        , position absolute
+        ]
     ]
         |> namespace replaceNamespace
         |> stylesheet
@@ -113,19 +149,8 @@ replaceNamespace =
 
 view : Model -> List (Html Msg)
 view model =
-    [ div
-        [ class [ ColorRow ] ]
-        [ p [] [ Html.text "replace" ]
-        , colorView model.target
-        ]
-    , div
-        [ class [ ColorRow ] ]
-        [ p [] [ Html.text "with" ]
-        , colorView model.replacement
-        ]
-    , div
-        [ class [ Palette ] ]
-        (squares model.palette)
+    [ replaceSide model
+    , withSide model
     , Html.Custom.menuButton
         [ class [ ReplaceButton ]
         , onClick ReplaceButtonClicked
@@ -134,19 +159,80 @@ view model =
     ]
 
 
-squares : List Color.Color -> List (Html Msg)
-squares =
-    List.map square
-
-
-square : Color.Color -> Html Msg
-square color =
+replaceSide : Model -> Html Msg
+replaceSide { targetHexField, target, palette, targetPaletteIndex } =
     div
-        [ class [ Square ]
+        [ class [ Section ] ]
+        [ div
+            [ class [ Row ] ]
+            [ p [] [ Html.text "replace" ] ]
+        , div
+            [ class [ Row ] ]
+            [ colorView target
+            , input
+                [ spellcheck False
+                , onInput UpdateTargetHexField
+                , value targetHexField
+                , class [ HexField ]
+                ]
+                []
+            ]
+        , div
+            [ class [ Palette ] ]
+            (squares targetPaletteIndex palette)
+        ]
+
+
+withSide : Model -> Html Msg
+withSide model =
+    div
+        [ class [ Section, With ] ]
+        [ div
+            [ class [ Row ] ]
+            [ p [] [ Html.text "with" ] ]
+        , div
+            [ class [ Row ] ]
+            [ colorView model.replacement
+            , input
+                [ spellcheck False
+                , onInput UpdateReplacementHexField
+                , value model.replacementHexField
+                , class [ HexField ]
+                ]
+                []
+            ]
+        , div
+            [ class [ Palette ] ]
+            (squares model.replacementPaletteIndex model.palette)
+        ]
+
+
+squares : Maybe Int -> List Color.Color -> List (Html Msg)
+squares maybeIndex =
+    List.indexedMap (square maybeIndex)
+
+
+square : Maybe Int -> Int -> Color.Color -> Html Msg
+square maybeSelected index color =
+    div
+        [ class (squareClass maybeSelected index)
         , onClick (SquareClicked color)
         , Util.background color
         ]
         []
+
+
+squareClass : Maybe Int -> Int -> List Class
+squareClass maybeSelected index =
+    case maybeSelected of
+        Just selectedIndex ->
+            if selectedIndex == index then
+                [ Square, Selected ]
+            else
+                [ Square ]
+
+        Nothing ->
+            [ Square ]
 
 
 colorView : Color.Color -> Html Msg
@@ -166,5 +252,15 @@ init : Color.Color -> Color.Color -> List Color.Color -> Model
 init target replacement palette =
     { target = target
     , replacement = replacement
+    , targetHexField =
+        target
+            |> Util.toHexColor
+            |> String.dropLeft 1
+    , replacementHexField =
+        replacement
+            |> Util.toHexColor
+            |> String.dropLeft 1
+    , targetPaletteIndex = Just 0
+    , replacementPaletteIndex = Just 2
     , palette = palette
     }
