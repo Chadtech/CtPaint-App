85c46a854d53578fcab6eae8465e20ad8d3a8e48
Chadtech <chadstearns1@gmail.com>
2018-04-15 16:06:33 +0200

    Simplified gulpfile

--- gulpFile.js
+++ gulpFile.js
@@ -14,25 +14,6 @@ paths = {
   js: "./source/**/*.js"
 };
 
-var waitApp = false;
-var waitStylesheet = false;
-
-function startWaiting(what) {
-  switch (what) {
-    case "app": 
-      waitApp = true;
-      break;
-
-    case "stylesheet":
-      waitStylesheet = true;
-      break;
-  }
-  setTimeout(function() {
-    waitApp = false;
-    waitStylesheet = false;
-  }, 3000);
-}
-
 gulp.task("js", function() {
   return browserify("./source/app.js")
     .bundle()
@@ -41,83 +22,34 @@ gulp.task("js", function() {
     .pipe(gulp.dest(paths.development));
 });
 
-gulp.task("elm", [ "elm-make", "elm-css" ]);
+gulp.task("elm", makeElm);
 
-gulp.task("elm-make", function() {
-  if (!waitApp) {
-    startWaiting("app");
-    cp.spawn("elm-make", [ 
-      paths.mainElm, 
-      "--warn", 
-      "--output", 
-      paths.development + "/paint-app-elm.js",
-      "--yes"
-    ], { 
-      stdio: 'inherit' 
-    }).on('close', function() {
-      util.log(util.colors.cyan("elm-make finished"));
-    });
-  //   startWaiting("app");
-  //   var cmd = [
-  //     "elm-make",
-  //     paths.mainElm,
-  //     "--warn",
-  //     "--output",
-  //     paths.development + "/paint-app-elm.js"
-  //   ].join(" ");
-  //   return cp.exec(cmd, function(error, stdout, stderr) {
-  //     if (error) {
-  //       error = (String(error)).slice(0, (String(error)).length - 1);
-  //       (error.split("\n")).forEach(function(line) {
-  //         return util.log(util.colors.red(String(line)));
-  //       });
-  //     } else {
-  //       stderr = stderr.slice(0, stderr.length - 1);
-  //       (stderr.split("\n")).forEach(function(line) {
-  //         return util.log(util.colors.yellow(String(line)));
-  //       });
-  //     }
-  //     stdout = stdout.slice(0, stdout.length - 1);
-  //     return (stdout.split("\n")).forEach(function(line) {
-  //       return util.log(util.colors.cyan("Elm"), line);
-  //     });
-  //   });
-  }
-});
+function elmCss() {
+  util.log(util.colors.cyan("Elm-Css"), "starting");
+  cp.spawn("elm-css", [ "./source/Stylesheets.elm" ], { 
+    stdio: 'inherit' 
+  }).on('close', function() {
+    util.log(util.colors.cyan("Elm-Css"), "closed");
+  });
+}
 
-
-gulp.task("elm-css", function() {
-  if (!waitStylesheet) {
-    startWaiting("stylesheet");
-    // cp.spawn("elm-css", [ "./source/Stylesheets.elm" ], { 
-    //   stdio: 'inherit' 
-    // }).on('close', function() {
-    //   util.log(util.colors.cyan("elm-css finished"));
-    // });
-    // var cmd = [
-    //   "elm-css",
-    //   "./source/Stylesheets.elm",
-    // ].join(" ");
-    // return cp.exec(cmd, function(error, stdout, stderr) {
-    //   if (error) {
-    //     error = (String(error)).slice(0, (String(error)).length - 1);
-    //     (error.split("\n")).forEach(function(line) {
-    //       util.log(util.colors.red(String(line)));
-    //     });
-    //   } else {
-    //     stderr = stderr.slice(0, stderr.length);
-    //     (stderr.split("\n")).forEach(function(line) {
-    //       util.log(util.colors.yellow(String(line)));
-    //     });
-    //   }
-    //   stdout = stdout.slice(0, stdout.length - 1);
-    //   (stdout.split("\n")).forEach(function(line) {
-    //     util.log(util.colors.cyan("Elm Css"), line);
-    //   });
-    // });
-  }
-});
-
+function makeElm() {
+  util.log(util.colors.cyan("Elm"), "starting");
+  cp.spawn("elm-make", [ 
+    paths.mainElm, 
+    "--warn", 
+    "--output", 
+    paths.development + "/paint-app-elm.js",
+    "--yes"
+  ], {
+    stdio: 'inherit'
+  }).on("close", function(code) {
+    util.log(util.colors.cyan("Elm"), "closed");
+    if (code === 0) {
+      elmCss()
+    };
+  });
+};
 
 gulp.task("server", function() {
   return (require("./server"))(2963, util.log);
